f1933ba3d87a5875d5c7ca6cafe2ef19ff7d331c
unknown <sergivb01@gmail.com>
2017-11-08 00:21:33 +0100

    Changed request/report messages (sender color => AQUA && staffserver now only server )

--- src/me/sergivb01/giraffe/commands/StaffServerCommand.java
+++ src/me/sergivb01/giraffe/commands/StaffServerCommand.java
@@ -3,7 +3,6 @@ package me.sergivb01.giraffe.commands;
 import me.sergivb01.giraffe.Giraffe;
 import net.minecraft.util.com.google.common.io.ByteArrayDataOutput;
 import net.minecraft.util.com.google.common.io.ByteStreams;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -25,21 +24,12 @@ public class StaffServerCommand implements CommandExecutor{
 
         Player player = (Player) sender;
 
-        if (args.length <= 1) {
-            player.sendMessage(ChatColor.RED + "Usage: /staffserver <server> <player_to_teleport>");
-            player.sendMessage(ChatColor.RED + "Example: " + ChatColor.GRAY + "/staffserver kits Hacker_ProGamer25");
-            return false;
-        }
 
-        Bukkit.dispatchCommand(player, "tp " + args[1]);
-
-        if(!args[0].equalsIgnoreCase(this.plugin.getServerName())) {
-            final ByteArrayDataOutput dataOutput = ByteStreams.newDataOutput();
-            dataOutput.writeUTF("Connect");
-            dataOutput.writeUTF(args[0]);
-            ((Player) sender).sendPluginMessage(plugin, "BungeeCord", dataOutput.toByteArray());
-            sender.sendMessage(ChatColor.YELLOW + "Sending you to " + ChatColor.GOLD + args[0]);
-        }
+        final ByteArrayDataOutput dataOutput = ByteStreams.newDataOutput();
+        dataOutput.writeUTF("Connect");
+        dataOutput.writeUTF(args[0]);
+        player.sendMessage(ChatColor.YELLOW + "Sending you to " + ChatColor.GOLD + args[0]);
+        player.sendPluginMessage(plugin, "BungeeCord", dataOutput.toByteArray());
 
         return true;
     }
--- src/me/sergivb01/giraffe/redis/DataSubscriber.java
+++ src/me/sergivb01/giraffe/redis/DataSubscriber.java
@@ -1,9 +1,9 @@
 package me.sergivb01.giraffe.redis;
 
+import me.sergivb01.giraffe.Giraffe;
 import net.veilmc.base.BasePlugin;
 import net.veilmc.util.chat.ClickAction;
 import net.veilmc.util.chat.Text;
-import me.sergivb01.giraffe.Giraffe;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -58,7 +58,7 @@ public class DataSubscriber
                             case "request": {
                                 for (final Player staff : Bukkit.getOnlinePlayers()) {
                                     if (staff.hasPermission("rank.staff") && BasePlugin.getPlugin().getUserManager().getUser(staff.getUniqueId()).isStaffChatVisible()) {
-                                        new Text(ChatColor.BLUE + "[Request] " + ChatColor.GRAY + "[" + server + "] " + ChatColor.RESET + sender + ChatColor.GRAY + " requested assistance.").setHoverText(ChatColor.BLUE + "Click here to teleport to " + ChatColor.GRAY + sender).setClick(ClickAction.RUN_COMMAND, "/staffserver " + server + " " + sender).send(staff);
+                                        new Text(ChatColor.BLUE + "[Request] " + ChatColor.GRAY + "[" + server + "] " + ChatColor.AQUA + sender + ChatColor.GRAY + " requested assistance.").setHoverText(ChatColor.BLUE + "Click here to teleport to " + ChatColor.GRAY + server).setClick(ClickAction.RUN_COMMAND, "/staffserver " + server).send(staff);
                                         staff.sendMessage(ChatColor.BLUE + "    Reason: " + ChatColor.GRAY + msg);
                                     }
                                 }
@@ -68,7 +68,7 @@ public class DataSubscriber
                                 final String target = args[4];
                                 for (final Player staff2 : Bukkit.getOnlinePlayers()) {
                                     if (staff2.hasPermission("rank.staff") && BasePlugin.getPlugin().getUserManager().getUser(staff2.getUniqueId()).isStaffChatVisible()) {
-                                        new Text(ChatColor.RED + "[Report] " + ChatColor.GRAY + "[" + server + "] " + ChatColor.RESET + sender + ChatColor.GRAY + " has reported " + ChatColor.RESET + target + ChatColor.GRAY + ".").setHoverText(ChatColor.RED + "Click here to teleport to " + ChatColor.GRAY + target).setClick(ClickAction.RUN_COMMAND, "/staffserver " + server + " " + target).send(staff2);
+                                        new Text(ChatColor.RED + "[Report] " + ChatColor.GRAY + "[" + server + "] " + ChatColor.AQUA + sender + ChatColor.GRAY + " has reported " + ChatColor.RESET + target + ChatColor.GRAY + ".").setHoverText(ChatColor.RED + "Click here to teleport to " + ChatColor.GRAY + server).setClick(ClickAction.RUN_COMMAND, "/staffserver " + server).send(staff2);
                                         staff2.sendMessage(ChatColor.RED + "    Reason:" + ChatColor.GRAY + msg.replace(target, ""));
                                     }
                                 }
